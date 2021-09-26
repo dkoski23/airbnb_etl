e4e2524be9c5025be08cfa97ab69dd9952db6470
dkoski23 <85533882+dkoski23@users.noreply.github.com>
2021-09-25 19:11:00 -0700

    Add files via upload
    The schema is up to date with what was loaded into the dataframe and the ERB code (in hastags at the bottom) in reading_etl_csvs is also up to date. The elt_project.ipynb reads the csvs, divides the data into the desired tables, cleans the data, and uses SQL Alchemy to connect and load the new tables into the database in pgAdmin.

--- etl_schema.sql
+++ etl_schema.sql
@@ -1,16 +1,15 @@
-DROP TABLE listing_info
+DROP TABLE listings_info
 DROP TABLE listing_review_stats
 DROP TABLE host_review_stats
 DROP TABLE host_location_review_stats
 DROP TABLE reviewer_freq
 DROP TABLE review_id
 
-CREATE TABLE listing_info(
+CREATE TABLE listings_info(
 	listing_id INT,
 	host_id INT,
-	name VARCHAR(40),
-	host_since DATE,
-	host_location VARCHAR(40),
+	name VARCHAR(400),
+	host_location VARCHAR(400),
 	minimum_nights INT,
 	maximum_nights INT,
 	instant_bookable BOOL,
@@ -21,19 +20,21 @@ CREATE TABLE listing_review_stats(
 	listing_id INT,
 	listing_rating FLOAT,
 	listing_score_accuracy FLOAT,
+	listing_score_cleanliness FLOAT,
 	listing_score_checkin FLOAT,
 	listing_score_communication FLOAT,
 	listing_score_location FLOAT,
 	listing_score_value FLOAT,
 	listing_total_reviews FLOAT,
-	FOREIGN KEY (listing_id) REFERENCES listing_info(listing_id),
+	FOREIGN KEY (listing_id) REFERENCES listings_info(listing_id),
 	PRIMARY KEY (listing_id)	
 );
 
 CREATE TABLE host_review_stats(
-	host_id INT,
+	host_id INT NOT NULL,
 	host_rating FLOAT,
 	host_score_accuracy FLOAT,
+	host_score_cleanliness FLOAT,
 	host_score_checkin FLOAT,
 	host_score_communication FLOAT,
 	host_score_location FLOAT,
@@ -43,27 +44,35 @@ CREATE TABLE host_review_stats(
 );
 
 CREATE TABLE host_location_review_stats(
-	host_location_id INT,
+	host_location VARCHAR(400),
 	host_location_rating FLOAT,
 	host_location_score_accuracy FLOAT,
+	host_location_score_cleanliness FLOAT,
 	host_location_score_checkin FLOAT,
 	host_location_score_communication FLOAT,
 	host_location_score_location FLOAT,
 	host_location_score_value FLOAT,
 	host_location_total_reviews FLOAT,
-	PRIMARY KEY (host_location_id)	
+	PRIMARY KEY (host_location)	
 );
 
 CREATE TABLE reviewer_freq(
 	reviewer_id INT,
-	listing_id INT,
-	FOREIGN KEY (listing_id) REFERENCES listing_info(listing_id),
+	total_reviews INT,
 	PRIMARY KEY (reviewer_id)
 );
 
 CREATE TABLE review_id(
 	review_id INT,
+	reviewer_id INT,
 	listing_id INT,
-	FOREIGN KEY (listing_id) REFERENCES listing_info(listing_id),
-	PRIMARY KEY (review_id)
+	FOREIGN KEY (reviewer_id) REFERENCES reviewer_freq(reviewer_id),
+	FOREIGN KEY (listing_id) REFERENCES listings_info(listing_id)
 )
+
+SELECT * FROM listings_info
+SELECT * FROM listing_review_stats
+SELECT * FROM host_review_stats
+SELECT * FROM host_location_review_stats
+SELECT * FROM reviewer_freq
+SELECT * FROM review_id
